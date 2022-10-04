06494964b7d81143caa88afd30448c73e327e506
Alexey <atayanchin@ppr.ru>
2022-10-04 18:51:09 +0300

    update readme

--- controllers/index.js
+++ controllers/index.js
@@ -68,7 +68,7 @@ router.post('/payment', async (req, res) => {
         return res.status(400).send({error: invoiceErr.toString()});
     }
 
-    res.redirect('/paymentForm/success.html');
+    res.redirect('/success.html');
 
 });
 
